f832e0c7f738d37e7adf5d8a4d493150590a4747
Inacio Nery <inacio.nery@liferay.com>
2016-02-29 14:06:43 -0300

    LPS-63457 Remove calls of DDMFormValuesToFieldsConverterUtil on DDM and DDL

--- modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/storage/test/StorageAdapterTest.java
+++ modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/storage/test/StorageAdapterTest.java
@@ -34,7 +34,7 @@ import com.liferay.dynamic.data.mapping.storage.StorageAdapterRegistryUtil;
 import com.liferay.dynamic.data.mapping.storage.StorageType;
 import com.liferay.dynamic.data.mapping.test.util.DDMFormTestUtil;
 import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
-import com.liferay.dynamic.data.mapping.util.DDMFormValuesToFieldsConverterUtil;
+import com.liferay.dynamic.data.mapping.util.DDMFormValuesToFieldsConverter;
 import com.liferay.dynamic.data.mapping.util.FieldsToDDMFormValuesConverter;
 import com.liferay.dynamic.data.mapping.util.impl.DDMImpl;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValuesValidationException;
@@ -97,6 +97,7 @@ public class StorageAdapterTest extends BaseDDMServiceTestCase {
 
 	@Before
 	public void setUp() throws Exception {
+		setUpDDMFormValuesToFieldsConverter();
 		setUpFieldsToDDMFormValuesConverter();
 	}
 
@@ -689,6 +690,13 @@ public class StorageAdapterTest extends BaseDDMServiceTestCase {
 		return sb.toString();
 	}
 
+	protected void setUpDDMFormValuesToFieldsConverter() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		_ddmFormValuesToFieldsConverter = registry.getService(
+			DDMFormValuesToFieldsConverter.class);
+	}
+
 	protected void setUpFieldsToDDMFormValuesConverter() {
 		Registry registry = RegistryUtil.getRegistry();
 
@@ -711,7 +719,7 @@ public class StorageAdapterTest extends BaseDDMServiceTestCase {
 		DDMFormValues actualDDMFormValues =
 			_jsonStorageAdapter.getDDMFormValues(classPK);
 
-		Fields actualFields = DDMFormValuesToFieldsConverterUtil.convert(
+		Fields actualFields = _ddmFormValuesToFieldsConverter.convert(
 			ddmStructure, actualDDMFormValues);
 
 		Assert.assertEquals(
@@ -724,6 +732,7 @@ public class StorageAdapterTest extends BaseDDMServiceTestCase {
 	private static StorageAdapter _jsonStorageAdapter;
 	private static Locale _ptLocale;
 
+	private DDMFormValuesToFieldsConverter _ddmFormValuesToFieldsConverter;
 	private FieldsToDDMFormValuesConverter _fieldsToDDMFormValuesConverter;
 
 }
