b6b4e7de35e5554ef710b2701c425a206e5890ff
Inacio Nery <inacio.nery@liferay.com>
2016-03-03 18:15:39 -0300

    LPS-63457 Regenerate

--- modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMStructureLocalService.java
+++ modules/apps/business-productivity/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMStructureLocalService.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Projection;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.model.PersistedModel;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.search.Indexable;
@@ -522,6 +523,10 @@ public interface DDMStructureLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<DDMStructure> getClassStructures(long companyId,
 		long classNameId, int start, int end);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public JSONArray getDDMFormFieldsJSONArray(DDMStructure ddmStructure,
+		java.lang.String script);
 
 	/**
 	* Returns the d d m structure with the primary key.
