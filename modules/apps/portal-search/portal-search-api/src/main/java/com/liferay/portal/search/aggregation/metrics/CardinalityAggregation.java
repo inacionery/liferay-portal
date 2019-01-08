f667553428b3ab215f24244437288bf2f762939e
Michael Han <michael.han@liferay.com>
2019-01-08 14:38:00 -0600

    add aggregation

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/metrics/CardinalityAggregation.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.aggregation.metrics;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.search.aggregation.AggregationVisitor;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public class CardinalityAggregation extends BaseMetricsAggregation {
+
+	public CardinalityAggregation(String aggregationName, String field) {
+		super(aggregationName, field);
+	}
+
+	@Override
+	public <T> T accept(AggregationVisitor<T> aggregationVisitor) {
+		return aggregationVisitor.visit(this);
+	}
+
+	public Integer getPrecisionThreshold() {
+		return _precisionThreshold;
+	}
+
+	public void setPrecisionThreshold(Integer precisionThreshold) {
+		_precisionThreshold = precisionThreshold;
+	}
+
+	private Integer _precisionThreshold;
+
+}
