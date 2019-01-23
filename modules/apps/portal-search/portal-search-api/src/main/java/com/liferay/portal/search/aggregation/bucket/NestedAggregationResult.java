62a17a8498578fa711c247ae1010c68f97412de5
Inácio Nery <inacio.nery@liferay.com>
2019-01-23 17:13:44 -0300

    LPS-88706 Add AggregationResults

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/bucket/NestedAggregationResult.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.aggregation.bucket;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.search.aggregation.AggregationResults;
+
+/**
+ * @author Inácio Nery
+ */
+@ProviderType
+public class NestedAggregationResult {
+
+	public NestedAggregationResult(
+		AggregationResults aggregationResults, long count) {
+
+		_aggregationResults = aggregationResults;
+		_count = count;
+	}
+
+	public AggregationResults getAggregationResults() {
+		return _aggregationResults;
+	}
+
+	public long getCount() {
+		return _count;
+	}
+
+	private final AggregationResults _aggregationResults;
+	private final long _count;
+
+}
