181910f45706e726046be3575a69d1b07a3ab2ac
Inácio Nery <inacio.nery@liferay.com>
2019-02-12 18:47:32 -0300

    LPS-90074 Implement getRolesWorkflowTasksPage

--- modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowTaskResourceImpl.java
+++ modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowTaskResourceImpl.java
@@ -14,9 +14,15 @@
 
 package com.liferay.headless.workflow.internal.resource.v1_0;
 
+import com.liferay.headless.workflow.dto.v1_0.WorkflowTask;
 import com.liferay.headless.workflow.resource.v1_0.WorkflowTaskResource;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +33,48 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WorkflowTaskResource.class
 )
 public class WorkflowTaskResourceImpl extends BaseWorkflowTaskResourceImpl {
+
+	@Override
+	public Page<WorkflowTask> getRolesWorkflowTasksPage(
+			Long rolesId, Pagination pagination)
+		throws Exception {
+
+		Long companyId = CompanyThreadLocal.getCompanyId();
+
+		return Page.of(
+			transform(
+				_workflowTaskManager.getWorkflowTasksByRole(
+					companyId, rolesId, null, pagination.getStartPosition(),
+					pagination.getEndPosition(), null),
+				this::_toWorkflowTask),
+			pagination,
+				_workflowTaskManager.getWorkflowTaskCountByRole(
+					companyId, rolesId, null));
+	}
+
+	private WorkflowTask _toWorkflowTask(
+			com.liferay.portal.kernel.workflow.WorkflowTask workflowTask)
+		throws Exception {
+
+		return new WorkflowTask() {
+			{
+				setCompleted(workflowTask.getCompletionDate() != null);
+				setDateCompleted(
+					workflowTask.getCompletionDate() != null ?
+						workflowTask.getCompletionDate().toString() : "");
+				setDateCreated(workflowTask.getCreateDate().toString());
+				setDefinitionName(workflowTask.getWorkflowDefinitionName());
+				setDescription(workflowTask.getDescription());
+				setDueDate(
+					workflowTask.getDueDate() != null ?
+						workflowTask.getDueDate().toString() : "");
+				setId(workflowTask.getWorkflowTaskId());
+				setName(workflowTask.getName());
+			}
+		};
+	}
+
+	@Reference
+	private WorkflowTaskManager _workflowTaskManager;
+
 }
