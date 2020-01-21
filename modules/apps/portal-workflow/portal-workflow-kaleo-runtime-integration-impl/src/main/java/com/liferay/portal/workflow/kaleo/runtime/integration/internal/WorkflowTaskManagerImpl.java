866df4b03003afd382bef8dcfecf8222cbd497e2
Inácio Nery <inacio.nery@liferay.com>
2020-01-21 10:30:52 -0300

    LPS-107370 If the task is completed, then there are no assignable users

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowTaskManagerImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowTaskManagerImpl.java
@@ -267,6 +267,10 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 				_kaleoTaskInstanceTokenLocalService.getKaleoTaskInstanceToken(
 					workflowTaskId);
 
+			if (kaleoTaskInstanceToken.isCompleted()) {
+				return Collections.emptyList();
+			}
+
 
 			Set<User> users = new TreeSet<>(new UserFirstNameComparator(true));
 
@@ -646,6 +650,10 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 			KaleoTaskInstanceToken kaleoTaskInstanceToken =
 				_kaleoTaskInstanceTokenLocalService.getKaleoTaskInstanceToken(
 					workflowTaskId);
+
+			if (kaleoTaskInstanceToken.isCompleted()) {
+				return false;
+			}
 
 			ExecutionContext executionContext = _createExecutionContext(
 				kaleoTaskInstanceToken);
