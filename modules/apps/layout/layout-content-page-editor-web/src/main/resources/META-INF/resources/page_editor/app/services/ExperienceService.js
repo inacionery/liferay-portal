brandonyee333/test

LPS-106617 setUsedWidgets

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/services/ExperienceService.js
@@ -14,6 +14,15 @@
 
 import serviceFetch from './serviceFetch';
 
+function getExperienceUsedPortletIds({body, config}) {
+	const {segmentsExperienceId} = body;
+	const {getExperienceUsedPortletsURL} = config;
+
+	return serviceFetch(config, getExperienceUsedPortletsURL, {
+		segmentsExperienceId
+	});
+}
+
 export default {
 	/**
 	 * Asks backend to create a new experience
@@ -38,7 +47,16 @@ export default {
 			segmentsEntryId
 		};
 
-		return serviceFetch(config, addSegmentsExperienceURL, payload);
+		return serviceFetch(config, addSegmentsExperienceURL, payload).then(
+			response => {
+				const {segmentsExperienceId} = response.segmentsExperience;
+
+				return getExperienceUsedPortletIds({
+					body: {segmentsExperienceId},
+					config
+				}).then(porletIds => ({...response, porletIds}));
+			}
+		);
 	},
 
 	/**
@@ -51,8 +69,15 @@ export default {
 	 * @param {string} options.config.deleteSegmentsExperienceURL Url of the backend service
 	 */
 	removeExperience({body, config}) {
-		const {fragmentEntryLinkIds, segmentsExperienceId} = body;
-		const {deleteSegmentsExperienceURL} = config;
+		const {
+			fragmentEntryLinkIds,
+			segmentsExperienceId,
+			selectedExperienceId
+		} = body;
+		const {
+			defaultSegmentsExperienceId,
+			deleteSegmentsExperienceURL
+		} = config;
 
 		const payload = {
 			deleteSegmentsExperience: true,
@@ -60,7 +85,27 @@ export default {
 			segmentsExperienceId
 		};
 
-		return serviceFetch(config, deleteSegmentsExperienceURL, payload);
+		if (selectedExperienceId === segmentsExperienceId) {
+			return serviceFetch(config, deleteSegmentsExperienceURL, payload);
+		}
+
+		return serviceFetch(config, deleteSegmentsExperienceURL, payload).then(
+			response => {
+				return getExperienceUsedPortletIds({
+					body: {segmentsExperienceId: defaultSegmentsExperienceId},
+					config
+				}).then(porletIds => ({...response, porletIds}));
+			}
+		);
+	},
+
+	selectExperience({body, config}) {
+		const {segmentsExperienceId} = body;
+
+		return getExperienceUsedPortletIds({
+			body: {segmentsExperienceId},
+			config
+		});
 	},
 
 	/**

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/reducers/selectExperience.js
@@ -12,18 +12,21 @@
  * details.
  */
 
-import {switchLayoutData, selectExperience} from './utils';
+import {switchLayoutData, selectExperience, setUsedWidgets} from './utils';
 
 function selectExperienceReducer(state, payload) {
 	let nextState = state;
 
-	const newExperience = payload;
+	const {porletIds, segmentsExperienceId} = payload;
 
 	nextState = switchLayoutData(nextState, {
 		currentExperienceId: nextState.segmentsExperienceId,
-		targetExperienceId: newExperience.segmentsExperienceId
+		targetExperienceId: segmentsExperienceId
 	});
-	nextState = selectExperience(nextState, newExperience.segmentsExperienceId);
+
+	nextState = setUsedWidgets(nextState, {porletIds});
+
+	nextState = selectExperience(nextState, segmentsExperienceId);
 
 	return nextState;
 }

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/reducers/utils.js
@@ -168,12 +168,59 @@ function removeLayoutDataItemById(state, segmentsExperienceId) {
 	};
 }
 
+/**
+ * Sets used widgets based on the portletIds array
+ * @param {!Array} widgets
+ * @param {{!Array} portletIds
+ * @return {Array}
+ * @review
+ */
+function updateUsedWidgets(widgets, portletIds) {
+	const filteredWidgets = [...widgets];
+
+	filteredWidgets.forEach(widgetCategory => {
+		const {categories = [], portlets = []} = widgetCategory;
+
+		widgetCategory.categories = updateUsedWidgets(categories, portletIds);
+		widgetCategory.portlets = portlets.map(portlet => {
+			if (
+				portletIds.indexOf(portlet.portletId) !== -1 &&
+				!portlet.instanceable
+			) {
+				portlet.used = true;
+			} else {
+				portlet.used = false;
+			}
+
+			return portlet;
+		});
+	});
+
+	return filteredWidgets;
+}
+
+/**
+ *
+ * @param {object} state
+ * @param {string} segmentsExperienceId
+ * @returns {object}
+ */
+function setUsedWidgets(state, {porletIds}) {
+	const updatedWidgets = updateUsedWidgets(state.widgets, porletIds);
+
+	return {
+		...state,
+		widgets: updatedWidgets
+	};
+}
+
 export {
 	addExperience,
 	deleteExperienceById,
 	removeLayoutDataItemById,
 	selectExperience,
 	setExperienceLock,
+	setUsedWidgets,
 	storeNewLayoutData,
 	switchLayoutData,
 	updateFragmentEntryLinksEditableValues

inacionery/liferay-portal

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/services/ExperienceService.js
@@ -14,6 +14,15 @@
 
 import serviceFetch from './serviceFetch';
 
+function getExperienceUsedPortletIds({body, config}) {
+	const {segmentsExperienceId} = body;
+	const {getExperienceUsedPortletsURL} = config;
+
+	return serviceFetch(config, getExperienceUsedPortletsURL, {
+		segmentsExperienceId
+	});
+}
+
 export default {
 	/**
 	 * Asks backend to create a new experience
@@ -38,7 +47,16 @@ export default {
 			segmentsEntryId
 		};
 
-		return serviceFetch(config, addSegmentsExperienceURL, payload);
+		return serviceFetch(config, addSegmentsExperienceURL, payload).then(
+			response => {
+				const {segmentsExperienceId} = response.segmentsExperience;
+
+				return getExperienceUsedPortletIds({
+					body: {segmentsExperienceId},
+					config
+				}).then(portletIds => ({...response, portletIds}));
+			}
+		);
 	},
 
 	/**
@@ -51,8 +69,15 @@ export default {
 	 * @param {string} options.config.deleteSegmentsExperienceURL Url of the backend service
 	 */
 	removeExperience({body, config}) {
-		const {fragmentEntryLinkIds, segmentsExperienceId} = body;
-		const {deleteSegmentsExperienceURL} = config;
+		const {
+			fragmentEntryLinkIds,
+			segmentsExperienceId,
+			selectedExperienceId
+		} = body;
+		const {
+			defaultSegmentsExperienceId,
+			deleteSegmentsExperienceURL
+		} = config;
 
 		const payload = {
 			deleteSegmentsExperience: true,
@@ -60,7 +85,27 @@ export default {
 			segmentsExperienceId
 		};
 
-		return serviceFetch(config, deleteSegmentsExperienceURL, payload);
+		if (selectedExperienceId === segmentsExperienceId) {
+			return serviceFetch(config, deleteSegmentsExperienceURL, payload);
+		}
+
+		return serviceFetch(config, deleteSegmentsExperienceURL, payload).then(
+			response => {
+				return getExperienceUsedPortletIds({
+					body: {segmentsExperienceId: defaultSegmentsExperienceId},
+					config
+				}).then(portletIds => ({...response, portletIds}));
+			}
+		);
+	},
+
+	selectExperience({body, config}) {
+		const {segmentsExperienceId} = body;
+
+		return getExperienceUsedPortletIds({
+			body: {segmentsExperienceId},
+			config
+		});
 	},
 
 	/**

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/reducers/selectExperience.js
@@ -12,18 +12,21 @@
  * details.
  */
 
-import {switchLayoutData, selectExperience} from './utils';
+import {switchLayoutData, selectExperience, setUsedWidgets} from './utils';
 
 function selectExperienceReducer(state, payload) {
 	let nextState = state;
 
-	const newExperience = payload;
+	const {portletIds, segmentsExperienceId} = payload;
 
 	nextState = switchLayoutData(nextState, {
 		currentExperienceId: nextState.segmentsExperienceId,
-		targetExperienceId: newExperience.segmentsExperienceId
+		targetExperienceId: segmentsExperienceId
 	});
-	nextState = selectExperience(nextState, newExperience.segmentsExperienceId);
+
+	nextState = setUsedWidgets(nextState, {portletIds});
+
+	nextState = selectExperience(nextState, segmentsExperienceId);
 
 	return nextState;
 }

modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/reducers/utils.js
@@ -168,12 +168,59 @@ function removeLayoutDataItemById(state, segmentsExperienceId) {
 	};
 }
 
+/**
+ * Sets used widgets based on the portletIds array
+ * @param {!Array} widgets
+ * @param {{!Array} portletIds
+ * @return {Array}
+ * @review
+ */
+function updateUsedWidgets(widgets, portletIds) {
+	const filteredWidgets = [...widgets];
+
+	filteredWidgets.forEach(widgetCategory => {
+		const {categories = [], portlets = []} = widgetCategory;
+
+		widgetCategory.categories = updateUsedWidgets(categories, portletIds);
+		widgetCategory.portlets = portlets.map(portlet => {
+			if (
+				portletIds.indexOf(portlet.portletId) !== -1 &&
+				!portlet.instanceable
+			) {
+				portlet.used = true;
+			} else {
+				portlet.used = false;
+			}
+
+			return portlet;
+		});
+	});
+
+	return filteredWidgets;
+}
+
+/**
+ *
+ * @param {object} state
+ * @param {string} segmentsExperienceId
+ * @returns {object}
+ */
+function setUsedWidgets(state, {portletIds}) {
+	const updatedWidgets = updateUsedWidgets(state.widgets, portletIds);
+
+	return {
+		...state,
+		widgets: updatedWidgets
+	};
+}
+
 export {
 	addExperience,
 	deleteExperienceById,
 	removeLayoutDataItemById,
 	selectExperience,
 	setExperienceLock,
+	setUsedWidgets,
 	storeNewLayoutData,
 	switchLayoutData,
 	updateFragmentEntryLinksEditableValues