f4af8280ed0343df86033e6e33fd3e44c9a6d07c
Inácio Nery <inacio.nery@liferay.com>
2020-06-08 16:24:09 -0300

    LPS-113350 | LPS-111266 SF

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/components/select-dropdown/SelectDropdown.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/components/select-dropdown/SelectDropdown.es.js
@@ -25,7 +25,10 @@ const SelectDropdown = ({
 	selectedValue,
 	stateProps,
 }) => {
-	const [fetchState, setFetchState] = useState({isLoading: true});
+	const [fetchState, setFetchState] = useState({
+		error: null,
+		isLoading: true,
+	});
 	const [items, setItems] = useState([]);
 
 	const selectRef = useRef();
@@ -34,7 +37,7 @@ const SelectDropdown = ({
 		const params = {keywords: '', page: -1, pageSize: -1, sort: ''};
 
 		setFetchState({
-			hasError: null,
+			error: null,
 			isLoading: true,
 		});
 
@@ -42,7 +45,7 @@ const SelectDropdown = ({
 			.then((data) => {
 				setItems(data.items);
 				setFetchState({
-					hasError: null,
+					error: null,
 					isLoading: false,
 				});
 
@@ -59,9 +62,9 @@ const SelectDropdown = ({
 					}
 				}
 			})
-			.catch((hasError) => {
+			.catch((error) => {
 				setFetchState({
-					hasError,
+					error,
 					isLoading: false,
 				});
 			});
